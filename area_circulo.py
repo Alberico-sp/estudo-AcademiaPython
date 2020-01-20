ce05d8a22a1ac45c86cc97b04c74454a61f2691e
Albérico Bezerra <alberico.sp@gmail.com>
2020-01-20 18:40:35 -0300

    Create area_circulo.py

---
+++ area_circulo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Recebe o número
+raio = float(input("Digite o raio do círculo"))
+
+# calcula a área  pi x num²
+area =  (raio ** 2) * 3.14159
+
+# Mostra o resultado
+print(f"a área do cículo do raio {raio} é {area:.2f}")
